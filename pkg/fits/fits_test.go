535595c7b162cea4e7582cb168bb16091c6f2de6
Michael J. Roberts <michael@observerly.com>
2022-05-09 10:51:03 +0100

    feat: Added NewFITSImage() to iris module.
    feat: Added NewFITSImage() to iris module.  Includes associated test suite for module export definition and expected output.

--- pkg/fits/fits_test.go
+++ pkg/fits/fits_test.go
@@ -1,1 +1,25 @@
 package iris
+
+import "testing"
+
+var img = NewFITSImage()
+
+func TestNewDefaultFITSImageHeaderEnd(t *testing.T) {
+	var got = img.Header.End
+
+	var want bool = false
+
+	if got != want {
+		t.Errorf("NewFITSImage() Header.End: got %v, want %v", got, want)
+	}
+}
+
+func TestNewDefaultFITSImageBScale(t *testing.T) {
+	var got = img.Bscale
+
+	var want float32 = 1
+
+	if got != want {
+		t.Errorf("NewFITSImage() Bscale: got %v, want %v", got, want)
+	}
+}
