48afb8261d580d07419083627ae45da33ab2c092
Michael J. Roberts <michael@observerly.com>
2022-10-05 19:49:18 +0100

    feat: Added (b *RGGBExposure) GetBayerMatrixOffset to the IRIS rggb module.
    feat: Added (b *RGGBExposure) GetBayerMatrixOffset to the IRIS rggb module. Includes associated test suite for module export definition and expected output.

--- pkg/iris/rggb.go
+++ pkg/iris/rggb.go
@@ -2,27 +2,49 @@ package iris
 
 import (
 	"bytes"
+	"fmt"
 	"image"
+	"strings"
 )
 
 type RGGBExposure struct {
-	Width  int
-	Height int
-	Raw    [][]uint32
-	Buffer bytes.Buffer
-	Image  *image.RGBA
+	Width             int
+	Height            int
+	ColourFilterArray string
+	Raw               [][]uint32
+	Buffer            bytes.Buffer
+	Image             *image.RGBA
 }
 
-func NewRGGBExposure(exposure [][]uint32, xs int, ys int) RGGBExposure {
+func NewRGGBExposure(exposure [][]uint32, xs int, ys int, cfa string) RGGBExposure {
 	img := image.NewRGBA(image.Rect(0, 0, xs, ys))
 
 	rggb := RGGBExposure{
-		Width:  xs,
-		Height: ys,
-		Raw:    exposure,
-		Buffer: bytes.Buffer{},
-		Image:  img,
+		Width:             xs,
+		Height:            ys,
+		ColourFilterArray: cfa,
+		Raw:               exposure,
+		Buffer:            bytes.Buffer{},
+		Image:             img,
 	}
 
 	return rggb
+}
+
+/**
+	Accepts a CFA (Color Filter Array) string, e.g., "RGGB" and returns the Bayering Matrix offset
+**/
+func (b *RGGBExposure) GetBayerMatrixOffset() (xOffset int32, yOffset int32, err error) {
+	switch strings.ToLower(b.ColourFilterArray) {
+	case "rggb":
+		return 0, 0, nil
+	case "grbg":
+		return 1, 0, nil
+	case "gbrg":
+		return 0, 1, nil
+	case "bggr":
+		return 1, 1, nil
+	default:
+		return 0, 0, fmt.Errorf("unknown color filter array string: %v", b.ColourFilterArray)
+	}
 }
