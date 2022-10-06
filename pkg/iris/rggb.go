1515707729227b1134e4409b0f53de6088e54422
Michael J. Roberts <michael@observerly.com>
2022-10-06 17:22:59 +0100

    feat: Added (b *RGGBExposure) DebayerBilinearInterpolation(xOffset int, yOffset int) to the IRIS rggb module.
    feat: Added (b *RGGBExposure) DebayerBilinearInterpolation(xOffset int, yOffset int) to the IRIS rggb module. Includes associated test suite for module export definition and expected output.

--- pkg/iris/rggb.go
+++ pkg/iris/rggb.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"image"
+	"image/color"
 	"strings"
 )
 
@@ -16,10 +17,10 @@ type RGGBExposure struct {
 	Image             *image.RGBA
 }
 
-func NewRGGBExposure(exposure [][]uint32, xs int, ys int, cfa string) RGGBExposure {
+func NewRGGBExposure(exposure [][]uint32, xs int, ys int, cfa string) *RGGBExposure {
 	img := image.NewRGBA(image.Rect(0, 0, xs, ys))
 
-	rggb := RGGBExposure{
+	return &RGGBExposure{
 		Width:             xs,
 		Height:            ys,
 		ColourFilterArray: cfa,
@@ -27,14 +28,12 @@ func NewRGGBExposure(exposure [][]uint32, xs int, ys int, cfa string) RGGBExposu
 		Buffer:            bytes.Buffer{},
 		Image:             img,
 	}
-
-	return rggb
 }
 
 /**
 	Accepts a CFA (Color Filter Array) string, e.g., "RGGB" and returns the Bayering Matrix offset
 **/
-func (b *RGGBExposure) GetBayerMatrixOffset() (xOffset int32, yOffset int32, err error) {
+func (b *RGGBExposure) GetBayerMatrixOffset() (xOffset int, yOffset int, err error) {
 	switch strings.ToLower(b.ColourFilterArray) {
 	case "rggb":
 		return 0, 0, nil
@@ -47,4 +46,62 @@ func (b *RGGBExposure) GetBayerMatrixOffset() (xOffset int32, yOffset int32, err
 	default:
 		return 0, 0, fmt.Errorf("unknown color filter array string: %v", b.ColourFilterArray)
 	}
+}
+
+/**
+	Perform Debayering w/ Bilinear Interpolation Technique
+**/
+func (b *RGGBExposure) DebayerBilinearInterpolation(xOffset int, yOffset int) error {
+	var raw []uint32
+
+	// Flatten the 2D Colour Filter Array array into a 1D array:
+	for _, a := range b.Raw {
+		raw = append(raw, a...)
+	}
+
+	w := uint32(b.Width)
+
+	h := uint32(b.Height)
+
+	xo := uint32(xOffset)
+
+	yo := uint32(yOffset)
+
+	// We need to ensure our images are of even pixel dimensions:
+	// Effectively, we're ignoring the last row and column of pixels in odd sized images:
+	x := w - xo & ^uint32(1)
+
+	y := h - yo & ^uint32(1)
+
+	R := make([]float32, int(x)*int(y))
+
+	G := make([]float32, int(x)*int(y))
+
+	B := make([]float32, int(x)*int(y))
+
+	// Perform Bi-Linear Interpolation on the Colour Filter Array:
+	for i := uint32(0); i < y; i += 2 {
+		for j := uint32(0); j < x; j += 2 {
+			// Obtain a Convolution in the Red channel:
+			R = BiLinearConvolveRedChannel(i, j, raw, R, w, h, xo, yo, x, y)
+			// Obtain a Convolution in the Green channel:
+			G = BiLinearConvolveGreenChannel(i, j, raw, G, w, h, xo, yo, x, y)
+			// Obtain a Convolution in the Blue channel:
+			B = BiLinearConvolveBlueChannel(i, j, raw, B, w, h, xo, yo, x, y)
+		}
+	}
+
+	// Stack The RGB channels into a single image:
+	for i := 0; i < b.Height; i++ {
+		for j := 0; j < b.Width; j++ {
+			b.Image.Set(i, j, color.RGBA{
+				R: uint8(R[i*b.Height+j]),
+				G: uint8(G[i*b.Height+j]),
+				B: uint8(B[i*b.Height+j]),
+				A: 255,
+			})
+		}
+	}
+
+	return nil
 }
