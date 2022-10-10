b95886e4312efeb182187d29b3328b375eb127ae
Michael J. Roberts <michael@observerly.com>
2022-10-10 15:39:47 +0100

    feat: Added (s *StarsExtractor) GetBrightPixels() to the IRIS photometry module.
    feat: Added (s *StarsExtractor) GetBrightPixels() to the IRIS photometry module. Includes associated test suite for module export definition and expected output.

--- pkg/photometry/stars.go
+++ pkg/photometry/stars.go
@@ -39,3 +39,29 @@ func NewStarsExtractor(exposure [][]uint32, xs int, ys int, radius float32) *Sta
 		Stars:     stars,
 	}
 }
+
+func (s *StarsExtractor) GetBrightPixels() []Star {
+	stars := s.Stars
+
+	for index, value := range s.Raw {
+		// If the value of the pixel is above the threshold, append it to the stars array:
+		if float32(value) > s.Threshold {
+			is := Star{Index: int32(index), Value: value, X: float32(index % s.Width), Y: float32(index / s.Width), Intensity: value, HFR: 1.0}
+
+			// Check if the star is within the radius of any other stars for optimisation:
+			if len(stars) > 0 {
+				star := stars[len(stars)-1]
+
+				// Replace previous bright star with current if it is perceived brighter:
+				if star.Y == is.Y && star.X >= is.X-float32(s.Radius) && star.Value <= is.Value {
+					stars[len(stars)-1] = is
+				}
+			}
+
+			// Append the star to the stars array:
+			stars = append(stars, is)
+		}
+	}
+
+	return stars
+}
