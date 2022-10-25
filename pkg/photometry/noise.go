320cfa84daa36734b1c5c726dd4b101b22fd37e2
Michael J. Roberts <michael@observerly.com>
2022-10-25 15:26:59 +0100

    feat: Added (n *NoiseExtractor) GetGaussianNoise() to the IRIS photometry module.
    feat: Added (n *NoiseExtractor) GetGaussianNoise() to the IRIS photometry module. Includes associated test suite for module export definition and expected output.

--- pkg/photometry/noise.go
+++ pkg/photometry/noise.go
@@ -1,5 +1,7 @@
 package photometry
 
+import "math"
+
 type NoiseExtractor struct {
 	Width  int
 	Height int
@@ -22,4 +24,61 @@ func NewNoiseExtractor(exposure [][]uint32, xs int, ys int) *NoiseExtractor {
 		Noise:  0,
 		Raw:    raw,
 	}
+}
+
+/*
+  GetGaussianNoise()
+
+  From J. Immerkær, “Fast Noise Variance Estimation”, Computer Vision and Image Understanding, Vol. 64, No. 2, pp. 300-302, Sep. 1996.
+*/
+func (n *NoiseExtractor) GetGaussianNoise() float64 {
+	// Weights for the 3x3 noise estimate kernel:
+	weight := []int32{
+		1, -2, 1,
+		-2, 4, -2,
+		1, -2, 1,
+	}
+
+	xs := int32(n.Width)
+
+	ys := int32(len(n.Raw)) / xs
+
+	// Offsets for the 3x3 kernel:
+	offset := []int32{
+		-xs - 1, -xs, -xs + 1,
+		-1, 0, 1,
+		xs - 1, xs, xs + 1,
+	}
+
+	// The accumulated noise throughout the image:
+	noise := 0.0
+
+	for y := int32(1); y < ys; y++ {
+		acc := 0.0
+		for x := int32(1); x < xs; x++ {
+			// Get pixel offset value:
+			i := y*xs + x
+
+			// Convolved value at pixel:
+			conv := float64(0)
+
+			// Convolve the pixel with the weight matrix:
+			for j, o := range offset {
+				if i+o >= 0 && i+o < int32(len(n.Raw)) {
+					conv += float64(weight[j]) * float64(n.Raw[i+o])
+				}
+			}
+
+			// Accumulate the convolved values
+			acc += math.Abs(conv)
+		}
+
+		// Accumulate the noise
+		noise += acc
+	}
+
+	// Calculate the fraction of the noise to apply to the image using a Gaussian distribution:
+	fr := math.Sqrt(0.5*math.Pi) / (6.0 * float64(xs-2) * float64(ys-2))
+
+	return fr * noise
 }
