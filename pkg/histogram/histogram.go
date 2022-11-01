a75fdd831d99f3a5ff25881c032c406759cc8079
Michael J. Roberts <michael@observerly.com>
2022-11-01 17:24:11 +0000

    feat: Added HistogramGray16(img *image.Gray16) to IRIS histogram module.
    feat: Added HistogramGray16(img *image.Gray16) to IRIS histogram module.

--- pkg/histogram/histogram.go
+++ pkg/histogram/histogram.go
@@ -6,7 +6,9 @@ import (
 	"github.com/observerly/iris/pkg/utils"
 )
 
-const hsize = 256
+const hsize8 = 256
+
+const hsize16 = 65535
 
 /*
   HistogramGray
@@ -15,15 +17,37 @@ const hsize = 256
 
   Returns an array of 256 uint64 values containing a distribution of the pixel values.
 */
-func HistogramGray(img *image.Gray) [hsize]uint64 {
+func HistogramGray(img *image.Gray) [hsize8]uint64 {
 	bounds := img.Bounds()
 
 	size := bounds.Size()
 
-	var res [hsize]uint64
+	var res [hsize8]uint64
 
 	utils.DeferForEachPixel(size, func(x, y int) {
 		pixel := img.GrayAt(x, y)
+		res[pixel.Y]++
+	})
+
+	return res
+}
+
+/*
+  HistogramGray16
+
+  Computes the histogram for a 16 bit grayscale image, and bins the pixel values according to their accumulated count.
+
+  Returns an array of 256 uint64 values containing a distribution of the pixel values.
+*/
+func HistogramGray16(img *image.Gray16) [hsize16]uint64 {
+	bounds := img.Bounds()
+
+	size := bounds.Size()
+
+	var res [hsize16]uint64
+
+	utils.DeferForEachPixel(size, func(x, y int) {
+		pixel := img.Gray16At(x, y)
 		res[pixel.Y]++
 	})
 
