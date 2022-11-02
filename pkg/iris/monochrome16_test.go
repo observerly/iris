5fb28d26d49bb1471cdac71230b9f26af3395836
Michael J. Roberts <michael@observerly.com>
2022-11-02 12:37:39 +0000

    feat: Added (m *Monochrome16Exposure) Preprocess() to IRIS monochrome module.
    feat: Added (m *Monochrome16Exposure) Preprocess() to IRIS monochrome module. Includes associated test suite for module export definition and expected output.

--- pkg/iris/monochrome16_test.go
+++ pkg/iris/monochrome16_test.go
@@ -1,6 +1,7 @@
 package iris
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
@@ -50,6 +51,128 @@ func TestNewMonochrome16ExposureGetBuffer(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("Expected no error when creating the output buffer, got %q", err)
+	}
+}
+
+func TestNewMonochrome16ExposurePreprocess4x4(t *testing.T) {
+	var ex = [][]uint32{
+		{6, 6, 6, 6},
+		{6, 7, 8, 8},
+		{7, 8, 9, 7},
+		{6, 7, 8, 6},
+	}
+
+	mono := NewMonochrome16Exposure(ex, 1, 4, 4)
+
+	var x int = mono.Width
+
+	var y int = mono.Height
+
+	if x != 4 {
+		t.Errorf("got %q, wanted %q", x, 4)
+	}
+
+	if y != 4 {
+		t.Errorf("got %q, wanted %q", y, 4)
+	}
+
+	buff, err := mono.Preprocess()
+
+	if err != nil {
+		t.Errorf("Expected the buffer to be created successfully, but got %q", err)
+	}
+
+	f, err := os.Create("4x4image.jpg")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			t.Errorf("Expected the image buffer to be saved successfully, but got %q", err)
+		}
+
+		// Clean up the file after we have finished with the test:
+		os.Remove("4x4image.jpg")
+	}()
+
+	n, err := f.Write(buff.Bytes())
+
+	if err != nil {
+		t.Errorf("Expected the image buffer to be saved successfully, but got %q", err)
+	}
+
+	fmt.Println(n)
+
+	if n >= 1024 {
+		t.Errorf("Expected the number of bytes to be approximately less than 1024, but got %v", n)
+	}
+}
+
+func TestNewMonochrome16ExposurePreprocess16x16(t *testing.T) {
+	var ex = [][]uint32{
+		{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+		{6, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 6},
+		{7, 8, 9, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 9, 8, 7},
+		{6, 7, 8, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 8, 7, 6},
+		{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+		{6, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 6},
+		{7, 8, 9, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 9, 8, 7},
+		{6, 7, 8, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 8, 7, 6},
+		{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+		{6, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 6},
+		{7, 8, 9, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 9, 8, 7},
+		{6, 7, 8, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 8, 7, 6},
+		{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+		{6, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 6},
+		{7, 8, 9, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 9, 8, 7},
+		{6, 7, 8, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 8, 7, 6},
+	}
+
+	mono := NewMonochrome16Exposure(ex, 1, 16, 16)
+
+	var x int = mono.Width
+
+	var y int = mono.Height
+
+	if x != 16 {
+		t.Errorf("got %q, wanted %q", x, 16)
+	}
+
+	if y != 16 {
+		t.Errorf("got %q, wanted %q", y, 16)
+	}
+
+	buff, err := mono.Preprocess()
+
+	if err != nil {
+		t.Errorf("Expected the buffer to be created successfully, but got %q", err)
+	}
+
+	f, err := os.Create("16x16image.jpg")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			t.Errorf("Expected the image buffer to be saved successfully, but got %q", err)
+		}
+
+		// Clean up the file after we have finished with the test:
+		os.Remove("16x16image.jpg")
+	}()
+
+	n, err := f.Write(buff.Bytes())
+
+	if err != nil {
+		t.Errorf("Expected the image buffer to be saved successfully, but got %q", err)
+	}
+
+	if n >= 1086 {
+		t.Errorf("Expected the number of bytes to be approximately less than 1086, but got %q", n)
 	}
 }
 
