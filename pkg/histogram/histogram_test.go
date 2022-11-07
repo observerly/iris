04b4164c689d4d9a1a016783099d1ea7352af879
Michael J. Roberts <michael@observerly.com>
2022-11-07 13:34:53 +0000

    test: Added TestNewHistogramGray16(t *testing.T) to histogram IRIS module.
    test: Added TestNewHistogramGray16(t *testing.T) to histogram IRIS module.

--- pkg/histogram/histogram_test.go
+++ pkg/histogram/histogram_test.go
@@ -2,6 +2,7 @@ package histogram
 
 import (
 	"image"
+	"image/color"
 	"image/jpeg"
 	"math"
 	"os"
@@ -50,6 +51,54 @@ func TestNewHistogramGray(t *testing.T) {
 	}
 
 	if max != 259110 {
+		t.Errorf("Histogram maximum is not 256")
+	}
+}
+
+func TestNewHistogramGray16(t *testing.T) {
+	f, err := os.Open("../../images/noise16.jpeg")
+
+	if err != nil {
+		t.Errorf("Error opening image: %s", err)
+	}
+
+	defer f.Close()
+
+	img, err := jpeg.Decode(f)
+
+	if err != nil {
+		t.Errorf("Error decoding image: %s", err)
+	}
+
+	bounds := img.Bounds()
+
+	gray := image.NewGray16(bounds)
+
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			lum := 0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b)
+			gray.Set(x, y, color.Gray16{uint16(lum)})
+		}
+	}
+
+	h := HistogramGray16(gray)
+
+	if len(h) != 65535 {
+		t.Errorf("Histogram length is not 256")
+	}
+
+	min, max := float64(h[0]), float64(h[0])
+	for _, val := range h {
+		min = math.Min(min, float64(val))
+		max = math.Max(max, float64(val))
+	}
+
+	if min != 0 {
+		t.Errorf("Histogram minimum is not 0")
+	}
+
+	if max != 50189 {
 		t.Errorf("Histogram maximum is not 256")
 	}
 }
