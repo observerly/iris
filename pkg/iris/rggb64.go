402f332c5cd2c0fb2b35939749b4e06d02605cb9
Michael J. Roberts <michael@observerly.com>
2022-11-12 13:48:42 +0000

    feat: Added (b *RGGB64Exposure) DebayerBilinearInterpolation() to rggb64 IRIS module.
    feat: Added (b *RGGB64Exposure) DebayerBilinearInterpolation() to rggb64 IRIS module. Includes associated test suite for module export definition and expected output.

--- pkg/iris/rggb64.go
+++ pkg/iris/rggb64.go
@@ -4,8 +4,12 @@ import (
 	"bytes"
 	"fmt"
 	"image"
+	"image/color"
 	"image/jpeg"
 	"strings"
+	"sync"
+
+	"github.com/observerly/iris/pkg/photometry"
 )
 
 type RGGB64Exposure struct {
@@ -65,4 +69,100 @@ func (b *RGGB64Exposure) GetBuffer(img *image.RGBA64) (bytes.Buffer, error) {
 	}
 
 	return buff, nil
+}
+
+/**
+	Perform Debayering w/ Bilinear Interpolation Technique
+**/
+func (b *RGGB64Exposure) DebayerBilinearInterpolation() error {
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+
+	R := make(chan []float32, b.Pixels)
+
+	G := make(chan []float32, b.Pixels)
+
+	B := make(chan []float32, b.Pixels)
+
+	errors := make(chan error, 3)
+
+	var raw []uint32
+
+	// Flatten the 2D Colour Filter Array array into a 1D array:
+	for _, row := range b.Raw {
+		raw = append(raw, row...)
+	}
+
+	w := uint32(b.Width)
+
+	h := uint32(b.Height)
+
+	xOffset, yOffset, err := b.GetBayerMatrixOffset()
+
+	if err != nil {
+		return err
+	}
+
+	xo := uint32(xOffset)
+
+	yo := uint32(yOffset)
+
+	// We need to ensure our images are of even pixel dimensions:
+	// Effectively, we're ignoring the last row and column of pixels in odd sized images:
+	x := w - xo & ^uint32(1)
+
+	y := h - yo & ^uint32(1)
+
+	// Perform Bi-Linear Interpolation on the Colour Filter Array:
+	go func() {
+		defer wg.Done()
+		// Obtain a Convolution in the Red channel:
+		red := photometry.BiLinearConvolveRedChannel(raw, w, h, xo, yo, x, y)
+		R <- red
+	}()
+
+	go func() {
+		defer wg.Done()
+		// Obtain a Convolution in the Green channel:
+		green := photometry.BiLinearConvolveGreenChannel(raw, w, h, xo, yo, x, y)
+		G <- green
+	}()
+
+	go func() {
+		defer wg.Done()
+		// Obtain a Convolution in the Blue channel:
+		blue := photometry.BiLinearConvolveBlueChannel(raw, w, h, xo, yo, x, y)
+		B <- blue
+	}()
+
+	go func() {
+		wg.Wait()
+		close(R)
+		close(G)
+		close(B)
+		close(errors)
+	}()
+
+	red, green, blue := <-R, <-G, <-B
+
+	b.R = red
+
+	b.G = green
+
+	b.B = blue
+
+	// Stack The RGB channels into a single image:
+	for j := 0; j < b.Height; j++ {
+		for i := 0; i < b.Width; i++ {
+			b.Image.Set(i, j, color.RGBA{
+				R: uint8(red[j*b.Width+i]),
+				G: uint8(green[j*b.Width+i]),
+				B: uint8(blue[j*b.Width+i]),
+				A: 255,
+			})
+		}
+	}
+
+	return nil
 }
--- pkg/iris/rggb64_test.go
+++ pkg/iris/rggb64_test.go
@@ -1,6 +1,9 @@
 package iris
 
-import "testing"
+import (
+	"image/jpeg"
+	"testing"
+)
 
 func TestNewRGGB64ExposureWidth(t *testing.T) {
 	rggb := NewRGGB64Exposure(ex, 1, 800, 600, "RGGB")
@@ -127,5 +130,45 @@ func TestNewRGGB64ExposureGetBuffer(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("Expected no error when creating the output buffer, got %q", err)
+	}
+}
+
+func TestNewRGGB64DebayerBilinearInterpolation(t *testing.T) {
+	var ex = [][]uint32{
+		{123, 6, 117, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+		{89, 123, 81, 123, 8, 128, 8, 8, 8, 8, 8, 8, 8, 8, 7, 6},
+		{123, 8, 82, 7, 89, 7, 97, 7, 111, 7, 7, 7, 7, 9, 8, 7},
+		{6, 123, 8, 129, 6, 114, 6, 6, 6, 6, 6, 6, 6, 8, 7, 6},
+		{87, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+		{6, 129, 8, 212, 8, 117, 8, 8, 8, 8, 8, 8, 8, 8, 7, 6},
+		{7, 111, 9, 7, 7, 7, 7, 7, 7, 7, 7, 121, 7, 9, 8, 7},
+		{102, 7, 8, 6, 111, 6, 6, 6, 6, 6, 6, 6, 6, 8, 7, 6},
+		{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+		{6, 7, 98, 8, 108, 8, 173, 8, 8, 123, 8, 8, 8, 8, 7, 6},
+		{7, 8, 9, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 9, 8, 7},
+		{6, 7, 109, 6, 105, 6, 6, 6, 6, 6, 6, 6, 6, 8, 7, 6},
+		{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+		{6, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 6},
+		{7, 8, 9, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 9, 121, 7},
+		{6, 7, 8, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 8, 7, 191},
+	}
+
+	rggb := NewRGGBExposure(ex, 1, 16, 16, "RGGB")
+
+	err := rggb.DebayerBilinearInterpolation()
+
+	if err != nil {
+		t.Errorf("Expected the debayering to be successful, but got %q", err)
+	}
+
+	// Encode the image as a JPEG:
+	err = jpeg.Encode(&rggb.Buffer, rggb.Image, &jpeg.Options{Quality: 100})
+
+	if err != nil {
+		t.Errorf("Expected the JPEG encoding to be successful, but got %q", err)
+	}
+
+	if err != nil {
+		t.Errorf("Expected to be able to preprocess the RGGB CFA image, but got %q", err)
 	}
 }
