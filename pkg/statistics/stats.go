4e29270a5df74f0e6375a4e758928e088333c536
Michael J. Roberts <michael@observerly.com>
2022-11-13 15:35:29 +0000

    feat: Added calcMeanStdDevVar(data []float32) to stats IRIS module.
    feat: Added calcMeanStdDevVar(data []float32) to stats IRIS module. Includes associated test suite for module export definition and expected output.

--- pkg/statistics/stats.go
+++ pkg/statistics/stats.go
@@ -1,5 +1,7 @@
 package stats
 
+import "math"
+
 func calcMinMeanMax(data []float32) (min float32, mean float32, max float32) {
 	mmin, mmean, mmax := float32(data[0]), float32(0), float32(data[0])
 
@@ -14,4 +16,25 @@ func calcMinMeanMax(data []float32) (min float32, mean float32, max float32) {
 	}
 
 	return mmin, float32(mmean / float32(len(data))), mmax
+}
+
+func calcMeanStdDevVar(data []float32) (mean float32, stddev float32, variance float32) {
+	xvar, mmean := float32(0), float32(0)
+
+	for _, v := range data {
+		mmean += v
+	}
+
+	mmean /= float32(len(data))
+
+	for _, x := range data {
+		diff := x - mmean
+		xvar += diff * diff
+	}
+
+	xvar /= float32(len(data))
+
+	stddev = float32(math.Sqrt(float64(xvar)))
+
+	return mmean, stddev, xvar
 }
