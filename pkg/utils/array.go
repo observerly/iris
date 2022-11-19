f2ca7a033450d6171ac55582f82964ed4796a40a
Michael J. Roberts <michael@observerly.com>
2022-11-19 12:38:21 +0000

    feat: Added DivideFloat32Array operation for []float32.
    feat: Added DivideFloat32Array operation for []float32. Includes associated test suite for module export definition and expected output.

--- pkg/utils/array.go
+++ pkg/utils/array.go
@@ -20,3 +20,28 @@ func SubtractFloat32Array(a, b []float32) []float32 {
 	return s
 }
 
+/**
+  Divide
+
+  Computes the element-wise division of arrays a and b, scaled
+  with bMean and stores in array d "divide", that is, d[i]=a[i]*bMax/b[i].
+**/
+func DivideFloat32Array(a, b []float32, bMax float32) []float32 {
+	if len(a) != len(b) {
+		panic("to subtract arrays must be of same length")
+	}
+
+	d := make([]float32, len(a))
+
+	for i := range d {
+		index := b[i]
+
+		if index <= 0 {
+			d[i] = a[i]
+		} else {
+			d[i] = a[i] * bMax / b[i]
+		}
+	}
+
+	return d
+}
--- pkg/utils/array_test.go
+++ pkg/utils/array_test.go
@@ -33,3 +33,61 @@ func TestSubtractABNotEqualLengthPanic(t *testing.T) {
 
 	SubtractFloat32Array(a, b)
 }
+
+func TestDivideAB(t *testing.T) {
+	a := []float32{2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+
+	b := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	d := DivideFloat32Array(a, b, 10)
+
+	if len(d) != len(a) {
+		t.Errorf("result should be of same length as a")
+	}
+
+	if d[0] != a[0]*10/b[0] {
+		t.Errorf("result should be %v, but got %v", a[0]*10/b[0], d[0])
+	}
+
+	if d[5] != a[5]*10/b[5] {
+		t.Errorf("result should be %v, but got %v", a[5]*10/b[5], d[5])
+	}
+}
+
+func TestDivideABDegenerate(t *testing.T) {
+	a := []float32{2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+
+	b := []float32{-1, -2, 3, 4, 5, -6, 7, 8, 9, 10}
+
+	d := DivideFloat32Array(a, b, 10)
+
+	if len(d) != len(a) {
+		t.Errorf("result should be of same length as a")
+	}
+
+	if d[0] != a[0] {
+		t.Errorf("result should be %v, but got %v", a[0], d[0])
+	}
+
+	if d[1] != a[1] {
+		t.Errorf("result should be %v, but got %v", a[1], d[1])
+	}
+
+	if d[5] != a[5] {
+		t.Errorf("result should be %v, but got %v", a[5], d[5])
+	}
+}
+
+func TestDivideABNotEqualLengthPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+
+	a := []float32{2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+
+	b := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+
+	DivideFloat32Array(a, b, 10)
+}
