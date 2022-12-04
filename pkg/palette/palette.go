904ca982f490c3acc03cdac544ffbbd99ac4e353
Michael J. Roberts <michael@observerly.com>
2022-12-04 12:10:08 +0000

    feat: Added combinePaletteChannel(channel []PaletteChannel) to palette IRIS module.
    feat: Added combinePaletteChannel(channel []PaletteChannel) to palette IRIS module. Includes associated test suite amends for module export definition and expected output.

--- pkg/palette/palette.go
+++ pkg/palette/palette.go
@@ -1,5 +1,11 @@
 package palette
 
+import (
+	"fmt"
+
+	"github.com/observerly/iris/pkg/utils"
+)
+
 type PaletteChannel struct {
 	Data     []float32
 	Fraction float32
@@ -10,4 +16,47 @@ type Palette struct {
 	R    []PaletteChannel
 	G    []PaletteChannel
 	B    []PaletteChannel
+}
+
+/**
+	combinePaletteChannel takes a slice of PaletteChannel and combines them
+	into a single slice of float32.
+
+	@param c []PaletteChannel - the slice of PaletteChannel to combine
+	@returns []float32, error - the combined slice of float32, and any error
+**/
+func combinePaletteChannel(channel []PaletteChannel) ([]float32, error) {
+	// Take each channel of the palette, and their respective constituents, and multiply them by the fraction:
+	fraction := float32(0.0)
+
+	data := make([][]float32, 0)
+
+	for _, c := range channel {
+		for k, v := range c.Data {
+			// Return an error to say that the fraction can not be larger than 1.0 for a given channel:
+			if c.Fraction > 1.0 {
+				return nil, fmt.Errorf("the fraction for the red channel can not be larger than 1.0")
+			}
+
+			c.Data[k] = v * c.Fraction
+		}
+
+		// Add the data to the reds slice:
+		data = append(data, c.Data)
+
+		fraction += c.Fraction
+	}
+
+	// Return an error to say that the combined fraction can not be larger than 1.0 for a given channel:
+	if fraction > 1.0 {
+		return nil, fmt.Errorf("the fraction for the red channel can not be larger than 1.0")
+	}
+
+	d, err := utils.MeanFloat32Arrays(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return d, nil
 }
