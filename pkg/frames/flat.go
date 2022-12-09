d9020b40e728f0e0cd228ae657dd93d6d5a66844
Michael J. Roberts <michael@observerly.com>
2022-12-09 11:29:58 +0000

    feat: Added NewMasterFlatFrame(frames []fits.FITSImage, ...) *MasterFlatFrame to frames IRIS module.
    feat: Added NewMasterFlatFrame(frames []fits.FITSImage, ...) *MasterFlatFrame to frames IRIS module. Includes associated test suite amends for module export definition and expected output.

--- pkg/frames/flat.go
+++ pkg/frames/flat.go
@@ -1,7 +1,10 @@
 package frames
 
 import (
+	"time"
+
 	"github.com/observerly/iris/pkg/fits"
+	"github.com/observerly/iris/pkg/utils"
 )
 
 type MasterFlatFrame struct {
@@ -10,4 +13,93 @@ type MasterFlatFrame struct {
 	Frames           []fits.FITSImage // The individual bias frames used to create the master bias frame
 	Combined         *fits.FITSImage  // The combined master bias frame
 	CreatedTimestamp int64
+}
+
+/*
+NewMasterFlatFrame()
+
+Creates a new master flat frame from a slice of flat frames.
+
+The idea of a flat frame is to take a series of exposures immediately preceding
+a telescope observation session. Their purpose is to correct for any uneven
+illumination on the CCD surface. Uneven illumination typically occurs as a result
+of vignetting due to the optical properties of the device or due to uneven
+background illumination in the exposure environment. Flat-fieds may also reveal
+areas of uneven gains or dark currents in the CCD detector, providing an
+indication of CCD Defects.
+
+The master flat frame is then created by taking the mean of all the flat frames.
+
+@retuns a new FITSImage containing the master flat frame.
+@see Image Calibration & Stack Woodhouse, C. (2017). The Astrophotography Manual. Taylor & Francis. p.203
+*/
+func NewMasterFlatFrame(frames []fits.FITSImage, naxis int32, naxis1 int32, naxis2 int32, adu int32, exposureTime float32) (*MasterFlatFrame, error) {
+	pixels := naxis1 * naxis2
+
+	// Create a slice of 2D data arrays from the slice of FITSImages
+	data := make([][]float32, len(frames))
+
+	for i, frame := range frames {
+		data[i] = frame.Data
+	}
+
+	// Create a new FITSImage from the master bias data
+	f := fits.NewFITSImage(
+		naxis,
+		naxis1,
+		naxis2,
+		adu,
+	)
+
+	// Combine the data arrays into a single array, by taking
+	// the mean of the total of all the frames for each pixel:
+	if len(frames) > 1 {
+		combined, err := utils.MeanFloat32Arrays(data)
+
+		if err != nil {
+			return nil, err
+		}
+
+		f.Data = combined
+	} else {
+		combined := frames[0].Data
+
+		f.Data = combined
+	}
+
+	f.Exposure = exposureTime
+
+	f.Pixels = pixels
+
+	f.Header.Ints["ADU"] = struct {
+		Value   int32
+		Comment string
+	}{
+		Value:   adu,
+		Comment: "Analog to Digital Units (ADU)",
+	}
+
+	f.Header.Floats["EXPOSURE"] = struct {
+		Value   float32
+		Comment string
+	}{
+		Value:   exposureTime,
+		Comment: "The exposure time (s) of the flat frame",
+	}
+
+	f.Header.Strings["SENSOR"] = struct {
+		Value   string
+		Comment string
+	}{
+		Value:   "Monochrome",
+		Comment: "ASCOM Alpaca Sensor Type",
+	}
+
+	return &MasterFlatFrame{
+		Count:            len(frames),
+		Pixels:           pixels,
+		Frames:           frames,
+		Combined:         f,
+		CreatedTimestamp: time.Now().Unix(),
+	}, nil
 }
---
+++ pkg/frames/flat_test.go
@@ -0,0 +1,64 @@
+package frames
+
+import (
+	"testing"
+
+	"github.com/observerly/iris/pkg/fits"
+)
+
+func TestNewMasterFlatFrame(t *testing.T) {
+	var flat = [][]uint32{
+		{255, 254, 255, 250, 255, 255, 255, 255, 255, 255, 255, 252, 253, 254, 252, 255},
+		{251, 250, 255, 255, 253, 252, 250, 255, 254, 250, 251, 251, 255, 255, 250, 255},
+		{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 250, 251, 255},
+		{250, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 253, 255, 255},
+		{255, 253, 252, 250, 255, 254, 250, 251, 251, 255, 255, 255, 254, 255, 255, 255},
+		{255, 252, 250, 255, 254, 250, 251, 251, 255, 255, 255, 255, 255, 254, 254, 255},
+		{255, 250, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+		{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+		{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+		{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+		{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+		{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+		{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+		{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+		{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+		{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+	}
+
+	var flatFrame = fits.NewFITSImageFrom2DData(flat, 2, 16, 16, 255)
+
+	var frames = []fits.FITSImage{
+		*flatFrame,
+		*flatFrame,
+		*flatFrame,
+		*flatFrame,
+		*flatFrame,
+	}
+
+	masterFlat, err := NewMasterFlatFrame(frames, 2, 16, 16, 255, 130)
+
+	if err != nil {
+		t.Errorf("NewMasterDarkFrame() failed: %s", err)
+	}
+
+	if masterFlat.Count != 5 {
+		t.Errorf("NewMasterFlatFrame() failed: expected count of 5, got %d", masterFlat.Count)
+	}
+
+	if masterFlat.Pixels != 256 {
+		t.Errorf("NewMasterFlatFrame() failed: expected pixels of 256, got %d", masterFlat.Pixels)
+	}
+
+	if masterFlat.Combined.ADU != 255 {
+		t.Errorf("NewMasterFlatFrame() failed: expected ADU of 255, got %d", masterFlat.Combined.ADU)
+	}
+
+	if masterFlat.Combined.Data[1] != 254 {
+		t.Errorf("NewMasterFlatFrame() failed: expected data[0] of 254, got %f", masterFlat.Combined.Data[0])
+	}
+
+	if masterFlat.Combined.Exposure != 130 {
+		t.Errorf("NewMasterFlatFrame() failed: expected exposure of 130, got %f", masterFlat.Combined.Exposure)
+	}
+}
