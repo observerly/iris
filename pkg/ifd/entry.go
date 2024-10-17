87f2b1069688b780192b458461f7cbab6299eff1
michealroberts <michael@observerly.com>
2024-10-17 18:31:06 +0100

    feat: add (e IFDEntry) PutData to metadata module in @observerly/iris
    feat: add (e IFDEntry) PutData to metadata module in @observerly/iris

--- pkg/ifd/entry.go
+++ pkg/ifd/entry.go
@@ -10,6 +10,10 @@ package metadata
 
 /*****************************************************************************************************************/
 
+import "encoding/binary"
+
+/*****************************************************************************************************************/
+
 // An IFDEntry is a single entry in an Image File Directory.
 // A value of type DataTypeRational is composed of two 32-bit values,
 // thus data contains two uints (numerator and denominator) for a single number.
@@ -17,6 +21,26 @@ type IFDEntry struct {
 	Tag      TagType
 	DataType DataType
 	Data     []uint32
+}
+
+/*****************************************************************************************************************/
+
+func (e IFDEntry) PutData(p []byte) {
+	enc := binary.LittleEndian
+
+	for _, d := range e.Data {
+		switch e.DataType {
+		case DataTypeByte, DataTypeASCII:
+			p[0] = byte(d)
+			p = p[1:]
+		case DataTypeShort:
+			enc.PutUint16(p, uint16(d))
+			p = p[2:]
+		case DataTypeLong, DataTypeRational:
+			enc.PutUint32(p, uint32(d))
+			p = p[4:]
+		}
+	}
 }
 
 /*****************************************************************************************************************/
